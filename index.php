3890d45c77be16a62285b270a1c68359d3bfa3fa
66070224 <66070224@kmitl.ac.th>
2026-05-29 17:48:58 +0700

    layout and card

--- index.php
+++ index.php
@@ -16,7 +16,9 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 
 
@@ -31,6 +33,63 @@
         color: #000000;
         text-decoration: none !important;
     }
+
+    .card {
+        border-radius: 5px;
+        padding: 10px;
+        box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1), 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        .card-header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            h5 {
+                margin: 0;
+                font-weight: bold;
+            }
+
+            @media (width<=320px) {
+                h5 {
+                    font-size: smaller;
+                }
+            }
+
+            a {
+                color: #337ab7;
+            }
+        }
+
+        .card-footer {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            a {
+                color: #337ab7;
+            }
+        }
+    }
+
+
+    .row>div {
+        margin-bottom: 20px;
+    }
+
+    section {
+        margin-bottom: 20px;
+    }
+
+    .section-1 {}
+
+    .section-2 {}
+
+    .section-3 {}
+
+    .section-4 {}
 </style>
 
 <body>
@@ -48,15 +107,120 @@
             </ul>
         </div>
     </nav>
-
-
-
-
-
     <div class="main-content">
+        <div class="container-fluid p-5">
 
-        <div class="container-fluid">
-            <div class="section1">
+            <section class="section-1 row">
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="card">TEST</div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="card">TEST</div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="card">TEST</div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="card">TEST</div>
+                </div>
+                <div class="col-lg-2 col-md-4 col-sm-6 col-12">
+                    <div class="card">TEST</div>
+                </div>
+            </section>
+
+            <section class="section-2 row">
+                <div class="col-lg-5 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>
+                                ความก้าวหน้าการเรียนของฉัน
+                            </h5>
+                        </div>
+                        <div class="card-body">BODY</div>
+                        <div class="card-footer"><a href="" class="btn">ดูทั้งหมด</a></div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>
+                                แผนการเรียนของฉัน (ตามตำแหน่งงาน)
+                            </h5>
+                        </div>
+                        <div class="card-body">BODY</div>
+                        <div class="card-footer"><a href="">ดูทั้งหมด</a></div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>
+                                บทเรียนที่ต้องเรียน / กำหนดเวลา
+                            </h5>
+                            <a href="">ดูทั้งหมด</a>
+                        </div>
+                        <div class="card-body">BODY</div>
+                        <div class="card-footer"><a href="">ดูทั้งหมด</a></div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-3 row">
+                <div class="col-lg-4 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>
+                                หลักสูตรที่ต้องเรียนต่อ
+                                <span class="badge" style="background-color: red; margin-left: 5px;">6</span>
+                            </h5>
+                        </div>
+                        <div class="card-body">BODY</div>
+                        <div class="card-footer"><a href="">ดูทั้งหมด</a></div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>
+                                หลักสูตรที่ไม่ผ่าน / ต้องซ่อม
+                                <span class="badge" style="background-color: red; margin-left: 5px;">2</span>
+                            </h5>
+                        </div>
+                        <div class="card-body">BODY</div>
+                        <div class="card-footer"><a href="">ดูทั้งหมด</a></div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>
+                                ผลประเมินล่าสุด
+                            </h5>
+                            <a href="">ดูทั้งหมด</a>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-4 row">
+                <div class="col-lg-4 col-12">
+                    <div class="card">TEST</div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="card">TEST</div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-12">
+                    <div class="card">TEST</div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
 <!-- 
                 <div>
                     <canvas id="myChart"></canvas>
@@ -122,12 +286,3 @@
 
                     </div>
                 </div> -->
-
-            </div>
-        </div>
-
-    </div>
-
-</body>
-
-</html>
